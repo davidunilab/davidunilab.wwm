48973076348caa9248648f135d3114fa08e5ccc4
David <Davidchincharashvili@gmail.com>
2021-08-30 23:13:59 +0400

    added meta tags

--- partitions/header.php
+++ partitions/header.php
@@ -1,15 +1,38 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible"
-    content="IE=edge">
-  <meta name="viewport"
-    content="width=device-width, initial-scale=1.0">
-  <title>Wonder Wonder Of Mathematics</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- TODO: change url -->
+    <!-- Primary Meta Tags -->
+    <title>მათემატიკის საოცარი სამყარო</title>
+    <meta name="title" content="მათემატიკის საოცარი სამყარო">
+    <meta name="description" content="მათამატიკია ახსნილია მარტივი ენით, დართული აქვს სავარჯიშოები, თავსატეხვი, თამაშები ასევე საიტი არის ილუსტრირებული. მის გამოყენება შეუძლიათ როგორც ბავშვებს, ასევერე უმაღლესი სასწავლებლის სტუდენტებს.">
+    <meta name="keywords" content="mathematics, algebra, geometry, topology, number theory ">
+    <meta name="robots" content="index, follow">
+    <meta name="language" content="English">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://metatags.io/">
+    <meta property="og:title" content="მათემატიკის საოცარი სამყარო">
+    <meta property="og:description" content="მათამატიკია ახსნილია მარტივი ენით, დართული აქვს სავარჯიშოები, თავსატეხვი, თამაშები ასევე საიტი არის ილუსტრირებული. მის გამოყენება შეუძლიათ როგორც ბავშვებს, ასევერე უმაღლესი სასწავლებლის სტუდენტებს.">
+    <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://metatags.io/">
+    <meta property="twitter:title" content="მათემატიკის საოცარი სამყარო">
+    <meta property="twitter:description" content="მათამატიკია ახსნილია მარტივი ენით, დართული აქვს სავარჯიშოები, თავსატეხვი, თამაშები ასევე საიტი არის ილუსტრირებული. მის გამოყენება შეუძლიათ როგორც ბავშვებს, ასევერე უმაღლესი სასწავლებლის სტუდენტებს.">
+    <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
   <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet"
-    href="//cdn.web-fonts.ge/fonts/bpg-glaho-bold/css/bpg-glaho-bold.min.css">
+  <link rel="stylesheet" href="//cdn.web-fonts.ge/fonts/bpg-glaho-bold/css/bpg-glaho-bold.min.css">
+
+
+
 </head>
 
 <body>
